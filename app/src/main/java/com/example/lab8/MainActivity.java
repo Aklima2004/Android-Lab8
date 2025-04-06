8ea3ceb55ca71f7289e09300368c36904ca82cf0
Aklima2004 <iskakbaeva.aklima@mail.ru>
2025-04-07 00:47:12 +0500

    Implementing MainActivity with BackgroundService functionality

--- app/src/main/java/com/example/lab8/MainActivity.java
+++ app/src/main/java/com/example/lab8/MainActivity.java
@@ -1,24 +1,57 @@
 package com.example.lab8;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText randomCharacterEditText;
+    private BroadcastReceiver broadcastReceiver;
+    private Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        randomCharacterEditText = findViewById(R.id.editText_randomCharacter);
+        serviceIntent = new Intent(this, RandomCharacterService.class); //ServiceRandomizer
+
+        // ✅ Регистрируем без флага, подходит для Android < 13
+        broadcastReceiver = new MyBroadcastReceiver();
+        IntentFilter filter = new IntentFilter("my.custom.action.tag.lab6");
+        registerReceiver(broadcastReceiver, filter);
+
+        Button startButton = findViewById(R.id.button_start);
+        Button stopButton = findViewById(R.id.button_stop);
+        Button nextButton = findViewById(R.id.button_next);
+
+        startButton.setOnClickListener(v -> startService(serviceIntent));
+        stopButton.setOnClickListener(v -> {
+            stopService(serviceIntent);
+            randomCharacterEditText.setText("");
         });
+        nextButton.setOnClickListener(v -> startActivity(new Intent(this, MainActivity2.class))); //SecondActivity
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    class MyBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            char data = intent.getCharExtra("randomCharacter", '?');
+            randomCharacterEditText.setText(String.valueOf(data));
+        }
     }
 }
