80d7d99ee7579caecd078058400c246a57794cab
Aklima2004 <iskakbaeva.aklima@mail.ru>
2025-04-07 01:28:04 +0500

    Updating settings to fix registerReceiver issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.example.lab8"
         minSdk = 31
-        targetSdk = 35
+        targetSdk = 32
         versionCode = 1
         versionName = "1.0"
 
